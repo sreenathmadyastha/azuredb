c0a5cd4da8ed38d523573191d1e5a5402f1b33f1
sreenathmadyastha <sreenathmadyastha>
2022-01-13 02:46:51 +0000

    Scala update

---
+++ ScalaDemo.scala
@@ -0,0 +1,14 @@
+// Databricks notebook source
+// MAGIC %md ### Exploration Notebook
+
+// COMMAND ----------
+
+"hello world"
+
+// COMMAND ----------
+
+var sum = 1+1
+
+// COMMAND ----------
+
+
